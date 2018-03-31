4e940ebd167a9f96370ddb2283e5e14f56042169
khromkov <khromkov@gmail.com>
2018-03-31 22:17:54 +0300

    test improve

--- src/test/WebSocket.test.js
+++ src/test/WebSocket.test.js
@@ -7,9 +7,48 @@ describe('WebSocket', () => {
     WebSocket.mock.clear();
   });
 
-  test('new WebSocket', () => {
-    new WebSocket('url'); // eslint-disable-line
+  test('create new', () => {
+    const ws = new WebSocket('url');
     expect(WebSocket.mock.instances).toHaveLength(1);
     expect(WebSocket.mock.instances[0].url).toBe('url');
+    expect(ws.readyState).toBe(WebSocket.CONNECTING);
+  });
+
+  test('open', () => {
+    const ws = new WebSocket('url');
+    const onopen = jest.fn();
+    ws.onopen = onopen;
+    ws.open();
+    expect(onopen).toHaveBeenCalledTimes(1);
+    expect(ws.readyState).toBe(WebSocket.OPEN);
+  });
+
+  test('close', () => {
+    const ws = new WebSocket('url');
+    const onclose = jest.fn(() => {
+      expect(ws.readyState).toBe(WebSocket.CLOSING);
+    });
+    ws.onclose = onclose;
+    ws.close();
+    expect(onclose).toHaveBeenCalledTimes(1);
+    expect(ws.readyState).toBe(WebSocket.CLOSED);
+  });
+
+  test('send', () => {
+    const ws = new WebSocket('url');
+    ws.send();
+    ws.close();
+    expect(() => {
+      ws.send();
+    }).toThrow();
+  });
+
+  test('without jest', () => {
+    global.WebSocket.isUseJest = false;
+    const ws = new WebSocket('url');
+    expect(ws.send.mock).toBe(undefined);
+    expect(ws.close.mock).toBe(undefined);
+    ws.send();
+    ws.close();
   });
 });
